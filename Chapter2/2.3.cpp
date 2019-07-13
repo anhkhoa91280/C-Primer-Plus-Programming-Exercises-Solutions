63baad96d8f1c3ee1129f1a7fd62447c482f9fae
Anh Khoa <anhkhoa91280@gmail.com>
2019-07-13 20:43:41 +0800

    add exercise 2.3 solution

---
+++ Chapter2/2.3.cpp
@@ -0,0 +1,23 @@
+/*
+ * Author: Dang Nguyen Anh Khoa
+ * Date: 20190713
+ * Desc: Program using 2 user-defined function and produces the following output:
+ *      Three blind mice
+ *      Three blind mice
+ *      See how they run
+ *      See how they run
+ * */
+#include <iostream>
+void print_mice();
+void print_how_mice_run();
+int main() {
+    print_mice();
+    print_how_mice_run();
+}
+void print_mice(){
+    std::cout << "Three blind mice" << std::endl;
+}
+
+void print_how_mice_run() {
+    std::cout << "See how they run" << std::endl;
+}
