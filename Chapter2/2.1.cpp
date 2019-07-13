bc672b6ae5386b06140ff34d26d48c633be0d08f
Anh Khoa <anhkhoa91280@gmail.com>
2019-07-13 17:28:01 +0800

    first commit

---
+++ Chapter2/2.1.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: Dang Nguyen Anh Khoa
+ * Date: 20190713
+ * Desc: Program displaying your name & address (fictitious name & address)
+ * */
+#include <iostream>
+
+int main() {
+    std::cout << "Name: Anh Khoa\nAddress: Hanoi,Vietnam" << std::endl; 
+}
