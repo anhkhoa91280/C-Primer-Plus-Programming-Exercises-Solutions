1db65d448157597fe4a250be49c249e85bfbff30
Anh Khoa <anhkhoa91280@gmail.com>
2019-07-13 18:31:39 +0800

    add exercise 2.2 solution

---
+++ Chapter2/2.2.cpp
@@ -0,0 +1,13 @@
+/*
+ * Author: Dang Nguyen Anh Khoa
+ * Date: 20190713
+ * Desc: Converts distance from furlongs to yards (1 furlong = 220 yards)
+ * */
+#include <iostream>
+
+int main() {
+    int distance_in_furlongs;
+    std::cout << "Enter a distance in furlong: ";
+    std::cin >> distance_in_furlongs;
+    std::cout << distance_in_furlongs<< " furlongs = " << distance_in_furlongs*220 << " yards" << std::endl;
+}
