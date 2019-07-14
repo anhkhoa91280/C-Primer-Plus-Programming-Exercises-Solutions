07b2e41f2dfba6f9c511c56197e542183253404d
Anh Khoa <anhkhoa91280@gmail.com>
2019-07-14 22:20:28 +0800

    Chapter 3: first commit

---
+++ Chapter3/limits.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: Dang Nguyen Anh Khoa
+ * Date: 20190714
+ * Desc: Check system integer type limits. Types are short, int, long, long long
+ * */
+#include <iostream>
+#include <climits>
+
+int main() {
+    short i_short = SHRT_MAX;
+    int i_int = INT_MAX;
+    long i_long = LONG_MAX;
+    long long i_long_long = LLONG_MAX; 
+
+    //sizeof operator yields size of type or size of variable
+    std::cout << "WIDTH OF EACH TYPE: " << std::endl;
+    std::cout << "short is: " << sizeof(short) << " bytes" << std::endl;
+    std::cout << "int is: " << sizeof i_int << " bytes" << std::endl;
+    std::cout << "long is: " << sizeof i_long << " bytes" << std::endl;
+    std::cout << "long long is: " << sizeof i_long_long << " bytes" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "MAXIMUM VALUE: " << std::endl;
+    std::cout << "short: " << i_short << std::endl;
+    std::cout << "int: " << i_int << std::endl;
+    std::cout << "long: " << i_long << std::endl;
+    std::cout << "long long: " << i_long_long << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "MINIMUM VALUES: " << std::endl;
+    std::cout << "bits per byte: " << CHAR_BIT << std::endl;
+    std::cout << "minimum int value: " << INT_MIN << std::endl;
+    std::cout << std::endl;
+    return 0;
+}
