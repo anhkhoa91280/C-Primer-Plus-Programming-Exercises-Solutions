e1f481ccd2a99fb53a7392fc311e1da818adf166
Anh Khoa <anhkhoa91280@gmail.com>
2019-07-14 20:32:17 +0800

    add exercise 2.7

---
+++ Chapter2/2.7.cpp
@@ -0,0 +1,21 @@
+/*
+ * Author: Dang Nguyen Anh Khoa
+ * Date: 20190714
+ * Desc: Program asks user for time and minute and display as hh : mm. 
+ *  The program should use a user-defined function that returns void.
+ * */
+#include <iostream>
+void print_hours_mins(int hours, int minutes);
+int main() {
+    int hours, minutes;
+    std::cout << "Enter hours: ";
+    std::cin >> hours;
+    std::cout << "Enter minutes: ";
+    std::cin >> minutes;
+
+    print_hours_mins(hours, minutes);
+    return 0;
+}
+void print_hours_mins(int hours, int minutes) {
+    std::cout << hours << ":" << minutes << std::endl;
+}
