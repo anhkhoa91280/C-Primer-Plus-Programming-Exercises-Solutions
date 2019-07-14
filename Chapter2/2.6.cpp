9aac4a2e156301a40b82714fd01a3cecfce4cd98
Anh Khoa <anhkhoa91280@gmail.com>
2019-07-14 20:19:40 +0800

    add exercise 2.6

---
+++ Chapter2/2.6.cpp
@@ -0,0 +1,19 @@
+/*
+ * Author: Dang Nguyen Anh Khoa
+ * Date: 20190714
+ * Desc: Program converts light years into astronomical units. 1 light year = 63240 astronomical units.
+ * */
+#include <iostream>
+double light_years_to_astronomical(double light_years);
+int main() {
+    double light_years;
+    std::cout << "Enter number of light years: ";
+    std::cin >> light_years;
+    std::cout << light_years << " light years = " << light_years_to_astronomical(light_years) 
+        << " astronomical units" << std::endl;
+    return 0;
+}
+
+double light_years_to_astronomical(double light_years) {
+    return light_years * 63240;
+}
