60dafc5d0af8b3386e798d283944b82a0dc3c0a9
khoa <anhkhoa91280@gmail.com>
2019-07-31 21:45:34 +0800

    update hexoct1.cpp

--- Chapter3/hexoct1.cpp
+++ Chapter3/hexoct1.cpp
@@ -5,6 +5,7 @@
  * number systems such as Decimal, Octal, Hexadecimal
  * Any number starting with 0 and followed by digits in range 0-7 is octal
  * Any number starting with 0x or 0X is hexadecimal
+ * By default, cout display integer literal in decimal
  * */
 #include <iostream>
 
