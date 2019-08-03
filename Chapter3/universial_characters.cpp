e65945d01c204bfe20fbe06871b97188f5cfb160
khoa <anhkhoa91280@gmail.com>
2019-08-03 22:08:43 +0800

    attempt to use char16_t and char32_t

--- Chapter3/universial_characters.cpp
+++ Chapter3/universial_characters.cpp
@@ -5,11 +5,15 @@
  * Reference: C++ Primer Plus - 6th Edition
  * */
 #include <iostream>
+//#include <unistr.h>
 
 int main() {
 	int k\u00F6rper;
-	//char hi = \u1ED2;
-	std::cout << "Let them eat g\u00E2teau.\n";
-	//std::cout << "oo in vietnamese: " << hi << std::endl;
+	std::cout << "Let them eat g\u00E2teau. \U0000222B\n";
 	std::cout << "\u00E4" << std::endl;
+
+	/*char16_t c1 = u'q';
+	char32_t c2{U'\U0000222B'};
+
+	std::cout << "c1: " << c1 << std::endl;*/
 }
