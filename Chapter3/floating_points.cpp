8c3a44bb2f76e35bcecfe460a22b2a4f1f50ec45
khoa <anhkhoa91280@gmail.com>
2019-08-05 19:06:16 +0800

    add more examples of floating point numbers

--- Chapter3/floating_points.cpp
+++ Chapter3/floating_points.cpp
@@ -10,7 +10,21 @@
 
 int main() {
 	//setf sets cout to display 6 fractional numbers
+	//This line is essential for displaying floating point numbers with 
+	//mantissa and exponent
 	std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+
+	double num0 = 1.6e+6;
+	double num1 = 1.6e-6;
+	double num2 = -1.6e-6;
+	std::cout << "Can use e or E, with or without + sign, etc" << std::endl;
+	std::cout << "Some examples of floating point numbers: " << std::endl;
+	std::cout << " 1.6e+6 = " << num0 << std::endl;
+	std::cout << "1.6e-6 = " << num1 << std::endl;
+	std::cout << "-1.6e-6 = " << num2 << std::endl;
+	
+	std::cout << std::endl;
+
 	float f_num = 10.0/3.0;
 	double d_num = 10.0/3.0;
 	const auto mil = 1.0e6;
