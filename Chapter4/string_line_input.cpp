5f5bcf406ea5aeb510220536d21a38e9eb4b87e9
khoa <anhkhoa91280@gmail.com>
2019-08-19 17:04:28 +0800

    edit string_line_input.cpp: determine lengths of uninitialized character array and string

--- Chapter4/string_line_input.cpp
+++ Chapter4/string_line_input.cpp
@@ -12,12 +12,20 @@ int main() {
 	char c_name[SIZE];
 	std::string s_name;
 
+	//The length of an uninitialized character array is random because strlen
+	//counts the number of bytes from 1st character until the first occurrence
+	//of a '\0' character
+	std::cout << "Character array length before entering name: " 
+		<< std::strlen(c_name) << std::endl;
 	std::cout << "Enter a name: ";
 	std::cin.getline(c_name, 10);
 	std::cout << "Character name entered: " << c_name << std::endl;
 	std::cout << "Character name has length: " << std::strlen(c_name) << std::endl;
 	std::cout << std::endl;
 
+	//The length of an uninitialized string is set to 0
+	std::cout << "String length before entering name: " 
+		<< s_name.size() << std::endl;
 	std::cout << "Enter another name: ";
 	getline(std::cin, s_name);
 	std::cout << "String name entered: " << s_name << std::endl;
