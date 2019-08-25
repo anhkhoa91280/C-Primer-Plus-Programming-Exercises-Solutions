b6cbb7723aecef93503e2bc6fead5f5ab42357de
khoa <anhkhoa91280@gmail.com>
2019-08-25 17:08:16 +0800

    add pointer.cpp

---
+++ Chapter4/pointer.cpp
@@ -0,0 +1,27 @@
+/*
+ * Name: Dang Nguyen Anh Khoa
+ * Date: 20190825
+ * Desc: This program shows C++ pointer
+ * Reference: C++ Primer Plus - 6th Edition
+ * */
+#include <iostream>
+
+int main() {
+	int balance = 20e3;
+	//* operator to declare the variable as of type pointer
+	int * p_balance = &balance;
+
+	//* operator can be used to dereference the value of the variable 
+	//residing the memory address that the pointer's pointing to
+	std::cout << "Balance: " << balance << std::endl;
+	std::cout << "Balance: " << *p_balance << std::endl;
+
+	std::cout << "p_balance: " << p_balance << std::endl;
+	std::cout << "p_balance: " << &balance << std::endl;
+
+	//use pointer to change the value of the variable it's pointing to
+	*p_balance = *p_balance + 5e3;
+	std::cout << "New balance: " << *p_balance << std::endl;
+
+	return 0;
+}
