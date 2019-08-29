c52cb7b9b522c8acd23e4607ea36d3cb42f2f86f
khoa <anhkhoa91280@gmail.com>
2019-08-29 18:44:32 +0800

    add file: pointer_dynamic_array.cpp

---
+++ Chapter4/pointer_dynamic_array.cpp
@@ -0,0 +1,22 @@
+/*
+ * Nqme: Dang Nguyen Anh Khoa
+ * Date: 20190829
+ * Desc: This program shows how to dynamically initialize an array using pointer
+ * Reference: C++ Primer Plus - 6th Edition
+ * */
+#include <iostream>
+
+int main() {
+	int * arr = new int[3];
+	arr[0] = 0;
+	arr[1] = 10;
+	arr[2] = 20;
+
+	std::cout << "arr[0]: " << arr[0] << std::endl; 
+	arr++;
+	std::cout << "arr[0] is now: " << arr[0] << std::endl; 
+	arr--;
+	std::cout << "arr[0] is now: " << arr[0] << std::endl; 
+
+	return 0;
+}
