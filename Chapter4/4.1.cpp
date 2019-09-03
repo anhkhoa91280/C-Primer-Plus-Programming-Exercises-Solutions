1335a322479673f5331050b60204e517225fd2f1
khoa <anhkhoa91280@gmail.com>
2019-09-03 15:07:29 +0700

    add file: 4.1.cpp

---
+++ Chapter4/4.1.cpp
@@ -0,0 +1,32 @@
+/*
+ * Name: Dang Nguyen Anh Khoa
+ * Date: 20190903
+ * Desc: This program asks for first name, last name, grade, and age. Then prints
+ * 	name as full name, grade down to 1 level, and age
+ * Reference: C++ Primer Plus - 6th Edition
+ * */
+#include <iostream>
+
+int main() {
+	std::string first_name, last_name, full_name;
+	char grade;
+	int age;
+
+	std::cout << "What's your first name? ";
+	std::cin >> first_name;
+
+	std::cout << "What's your last name? ";
+	std::cin >> last_name;
+
+	std::cout << "What's letter grade do you deserve? ";
+	std::cin >> grade;
+
+	std::cout << "What's your age ";
+	std::cin >> age;
+
+	std::cout << "Name: " << last_name << ", " << first_name << std::endl;
+	std::cout << "Grade: " << static_cast<char>(grade+1) << std::endl;
+	std::cout << "Age: " << age << std::endl;
+
+	return 0;
+}
