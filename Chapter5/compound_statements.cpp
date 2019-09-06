18cbb0c3d27d0e1e2ff2c553c83028bee415360b
khoa <anhkhoa91280@gmail.com>
2019-09-06 21:12:35 +0700

    edit: same variable name inside and outside the block

--- Chapter5/compound_statements.cpp
+++ Chapter5/compound_statements.cpp
@@ -9,13 +9,15 @@
 int main() {
 	int a = 10;
 	
+	std::cout << "a outside the block = " << a << std::endl;
 	{
+		int a = 5;
 		int b = 20;
 		std::cout << "a inside the block = " << a << std::endl;
 		std::cout << "b inside the block = " << b << std::endl;
 	}
 
-	std::cout << "a outside the block = " << a << std::endl;
+	std::cout << "a outside the block again = " << a << std::endl;
 	//the following piece of code won't compile because b is declared in the 
 	//block and it's not available outside the block
 	//std::cout << "b outside the block = " << b << std::endl;
