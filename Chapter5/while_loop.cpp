a4fffd3a481cbea589ed3bbc179af58d730d35e0
khoa <anhkhoa91280@gmail.com>
2019-09-07 20:02:48 +0700

    add file: while_loop.cpp

---
+++ Chapter5/while_loop.cpp
@@ -0,0 +1,22 @@
+/*
+ * Name: Dang Nguyen Anh Khoa
+ * Date: 20190907
+ * Desc: This program shows a while loop in C++
+ * Reference: C++ Primer Plus - 6th Edition
+ * */
+#include <iostream>
+
+int main() {
+	int i = -1;
+	std::string name;
+
+	std::cout << "Enter your name: ";
+	getline(std::cin, name);
+
+	std::cout << "Hello " << name << ", here's your name vertically and "
+		<< "ASCIIized" << std::endl;	
+	while ( name[++i] != '\0') 
+		std::cout << name[i] << ": " << static_cast<int>(name[i]) << std::endl;
+
+	return 0;
+}
