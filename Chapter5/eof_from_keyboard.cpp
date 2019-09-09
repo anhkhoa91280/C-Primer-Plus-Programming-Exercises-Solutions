8c5d9a4fd883e4d4b2be25db5cbfc37e0610e1b3
khoa <anhkhoa91280@gmail.com>
2019-09-09 15:31:27 +0700

    edit: add more variations to catch eof

--- Chapter5/eof_from_keyboard.cpp
+++ Chapter5/eof_from_keyboard.cpp
@@ -1,7 +1,8 @@
 /*
  * Name: Dang Nguyen Anh Khoa
  * Date: 20190909
- * Desc: This program shows how to catch eof from keyboard
+ * Desc: This program shows how to catch eof from keyboard. 
+ * 	There are many variations to do this action
  * Reference: C++ Primer Plus - 6th Edition
  * */
 #include <iostream>
@@ -10,12 +11,28 @@ int main() {
 	char c;
 
 	std::cout << "Enter some characters plz: ";
-	std::cin.get(c);
 
-	while ( std::cin.fail() == false ) {
+	//std::cin.get(c);
+	//while ( std::cin.fail() == false ) {
+	/*while ( std::cin.eof() == false ) {
 		std::cout << c;
 		std::cin.get(c);
+	}*/
+	//OR
+	std::cin.get(c);
+	while ( c != EOF ) {
+		std::cout.put(c);
+		std::cin.get(c);
 	}
+	//OR
+	//std::cin.get(c);
+	/*do {
+		std::cout << c;
+	} while ( std::cin.get(c) );*/
+
+	//OR
+	/*while ( std::cin.get(c) )
+		std::cout << c;*/
 
 	return 0;
 }
