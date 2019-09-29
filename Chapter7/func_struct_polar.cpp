2a1cd17a5826fafdefbe9e3ccc138b305f0969f6
khoa <anhkhoa91280@gmail.com>
2019-09-29 12:48:33 +0700

    add file: func_struct_polar.cpp

--- Chapter7/func_struct_polar.cpp
+++ Chapter7/func_struct_polar.cpp
@@ -16,31 +16,34 @@ struct Polar {
 	double distance;
 	double angle;
 };
-Polar rect_to_polar(Rect r);
-void show_polar(Polar p);
+Polar * rect_to_polar(const Rect * r);
+void show_polar(const Polar * p);
 int main() {
 	Rect r;
+	Polar * p = new Polar;
 
 	std::cout << "Enter x & y coordinates: ";
 	while ( std::cin >> r.x >> r.y ) {
-		show_polar(rect_to_polar(r));
+		p = rect_to_polar(&r);
+		show_polar(p);
+		delete p;
 		std::cout << "Another x & y coordinates combination or q to quit: ";
 	}
 
 	return 0;
 }
-Polar rect_to_polar(Rect r) {
-	Polar p;
+Polar * rect_to_polar(const Rect * r) {
+	Polar * p = new Polar;
 
-	p.distance = sqrt(pow(r.x, 2) + pow(r.y, 2));
-	p.angle = atan2(r.y, r.x);
+	p->distance = sqrt(pow(r->x, 2) + pow(r->y, 2));
+	p->angle = atan2(r->y, r->x);
 
 	return p;
 }
-void show_polar(Polar p) {
+void show_polar(const Polar * p) {
 	double rad_to_deg_ratio = 57.29577951;
 	
-	std::cout << "Distance: " << p.distance << std::endl;
-	std::cout << "Angle: " << p.angle * rad_to_deg_ratio 
+	std::cout << "Distance: " << p->distance << std::endl;
+	std::cout << "Angle: " << p->angle * rad_to_deg_ratio 
 		<< " degrees." << std::endl;
 }
