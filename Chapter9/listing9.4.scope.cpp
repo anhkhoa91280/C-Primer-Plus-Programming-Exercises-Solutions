a736e4b3ddd1388881d847fea8f6ba0097d462b4
khoa <anhkhoa91280@gmail.com>
2019-10-28 20:07:25 +0700

    add file: listing9.4.scope.cpp

---
+++ Chapter9/listing9.4.scope.cpp
@@ -0,0 +1,41 @@
+/*
+ * Name: Dang Nguyen Anh Khoa
+ * Date: 20191028
+ * Desc: This program explains scope of variables within functions, block, etc.
+ * Reference: C++ Primer Plus - 6th Edition.
+ * */
+#include <iostream>
+
+void g(int &, int &);
+int main() {
+	int x = 10;
+	int y = 20;
+
+	std::cout << "In main(), x: " << x << ", &x: " << &x << std::endl;  
+	std::cout << "In main(), y: " << y << ", &y: " << &y << std::endl;  
+
+	g(x, y);
+
+	return 0;
+}
+void g(int & param_x, int & param_y) {
+	int x = -10;
+	int y = -20;
+
+	std::cout << "In g(), x: " << x << ", &x: " << &x << std::endl; 
+	std::cout << "In g(), y: " << y << ", &y: " << &y << std::endl; 
+
+	std::cout << "In g(), param_x: " << param_x << ", &param_x: " 
+		<< &param_x << std::endl; 
+	std::cout << "In g(), param_y: " << param_y << ", &param_y: " 
+		<< &param_y << std::endl; 
+	{
+		int x = 1000;
+		int y = 2000;
+
+		std::cout << "In block, x: " << x << ", &x: " << &x << std::endl; 
+		std::cout << "In block, y: " << y << ", &y: " << &y << std::endl; 
+	}
+	std::cout << "After block, x: " << x << ", &x: " << &x << std::endl; 
+	std::cout << "After block, y: " << y << ", &y: " << &y << std::endl; 
+}
